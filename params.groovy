70c01f1f544c575bc8d29e47502663ec70ee91c1
vgopinad <33896185+vgopinad@users.noreply.github.com>
2021-12-29 13:08:10 +0530

    Update params.groovy

--- params.groovy
+++ params.groovy
@@ -4,9 +4,13 @@ node{
     deleteDir()
     
     def name=params.name1
-    
     print "value from jennkin ${name}"
    
+    config= readYaml file= "config/app.yml"
+    
+    def urlValue=config.url.create_org
+    
+    print "urlValue: ${urlValue} "
         
         }
   
